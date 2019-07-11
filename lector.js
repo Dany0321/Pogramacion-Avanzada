cbf477f9a312db3aa7bf9f25363888c5e24d088b
Javier Aponte <48999640+InsertCreativeNameHere@users.noreply.github.com>
2019-07-10 21:06:24 -0500

    ver 1.0
    primera vez que funciona bien xD

--- lector.js
+++ lector.js
@@ -6,9 +6,9 @@ var esclavo = new Worker('lectorMensajero.js')
 
 
 esclavo.onmessage = function(oEvent){
-    nombre = oEvent.data[0];
-    contra = oEvent.data[1];
-    window.location.replace("http://localhost/Cajero/Cuenta.html");
+    nombrelocal = oEvent.data[0];
+    window.alert(nombrelocal);
+    window.location.replace("http://localhost/Cajero/Cuenta.html?nombreE="+nombrelocal);
 }
 
 
